f6d1c7144e32af9a6db662c0848862fb5bebcff1
Shugmi Shumunov <SShumunov@gmail.com>
2021-10-19 23:08:57 -0400

    feat: Add initial version of Five Day Forecast page
    - Add ThreeHourForecast Component

--- src/Components/Pages/FiveDayForecast.tsx
+++ src/Components/Pages/FiveDayForecast.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { ThreeHourForecast } from '../Weather/ThreeHourForecast';
 
 interface FiveDayForecastPageProps {
 	currentForecast: any;
@@ -6,13 +7,29 @@ interface FiveDayForecastPageProps {
 
 export const FiveDayForecastPage: FC<FiveDayForecastPageProps> = (props) => {
 	const { currentForecast } = props;
-
 	return(
 		<div className="five-day-forecast-page">
 			<h1>Five Day Forecast</h1>
-			<pre>
-				{JSON.stringify(currentForecast, null, 2)}
-			</pre>
+			{currentForecast && currentForecast.map((threeHourBlock: any) => {
+				const forecastPeriod = threeHourBlock.dt_txt;
+				const maxTemperature = threeHourBlock.main.temp_max;
+				const minTemperature = threeHourBlock.main.temp_min;
+				const temperatureIcon = threeHourBlock.weather[0].icon;
+				const temperatureValue = threeHourBlock.main.temp;
+				const weatherDescription = threeHourBlock.weather[0].description;
+
+				return(
+					<ThreeHourForecast
+						key={threeHourBlock.dt}
+						forecastPeriod={forecastPeriod}
+						maxTemperature={maxTemperature}
+						minTemperature={minTemperature}
+						temperatureIcon={temperatureIcon}
+						temperatureValue={temperatureValue}
+						weatherDescription={weatherDescription}
+					/>
+				)
+			})}
 		</div>
 	);
 };
---
+++ src/Components/Weather/ThreeHourForecast.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import { getCurrentWeatherIcon } from '../../Services/WeatherService';
+
+interface ThreeHourForecastProps {
+	forecastPeriod: string;
+	maxTemperature: number;
+	minTemperature: number;
+	temperatureIcon: string;
+	temperatureValue: number;
+	weatherDescription: string;
+}
+
+export const ThreeHourForecast: FC<ThreeHourForecastProps> = (props) => {
+	const {
+		forecastPeriod,
+		maxTemperature,
+		minTemperature,
+		temperatureIcon,
+		temperatureValue,
+		weatherDescription
+	} = props;
+	const iconURL = getCurrentWeatherIcon(temperatureIcon);
+	return(
+		<div className="current-temperature-container">
+			<h2>Forecast for {forecastPeriod}</h2>
+			<div className="current-temp-info">
+				<p>
+					Description: {weatherDescription}
+					<span>
+						<img
+							src={iconURL}
+							alt="Current Weather Icon"
+						/>
+					</span>
+				</p>
+			</div>
+			<ul>
+				<li>Expected Temperature: {temperatureValue}&deg;F</li>
+				<li>Expected High: {maxTemperature}&deg;F</li>
+				<li>Expected Low: {minTemperature}&deg;F</li>
+			</ul>
+		</div>
+	);
+};
